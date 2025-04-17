18ce0c97b35efb593f19c6181ac2645f25208d3a
jbrugger <wwjbrugger@gmail.com>
2025-04-17 10:58:39 +0200

    add try except block around histogram calculation

--- src/analyse_equations/analyse_equations.py
+++ src/analyse_equations/analyse_equations.py
@@ -185,26 +185,32 @@ def abs_difference_between_equation(args, df, filtered_dfs_test, index_0, index_
 
 
 def histogram_for_features(args, filtered_dfs_test, tree):
-    y_pred = tree.evaluate_subtree(-1, filtered_dfs_test)
-    diff = y_pred - filtered_dfs_test['y']
-    fig, axs = plt.subplots(nrows=math.ceil(len(args.features) / 2), ncols=2)
-    for i in range(math.ceil(len(args.features) / 2) * 2):
-        ax = axs[int(i / 2), i % 2]
-        if i >= len(args.features):
-            ax.clear()
-        else:
-            feature = args.features[i]
+    try:
+        y_pred = tree.evaluate_subtree(-1, filtered_dfs_test)
+        diff = y_pred - filtered_dfs_test['y']
+        fig, axs = plt.subplots(nrows=math.ceil(len(args.features) / 2), ncols=2)
+        for i in range(math.ceil(len(args.features) / 2) * 2):
+            ax = axs[int(i / 2), i % 2]
+            if i >= len(args.features):
+                ax.clear()
+            else:
+                feature = args.features[i]
 
-        ax.hist2d(filtered_dfs_test[feature], diff, bins=10,
-                  # norm='log',
-                  cmap='YlGn',
-                  vmax=400)
-        ax.set_ylabel('$y_{pred}$ - $\\tilde y$')
-        ax.set_xlabel(feature)
-    fig.suptitle(f"{args.target} = {tree.rearrange_equation_infix_notation()[1]}", fontsize=10,
-                 )
-    fig.tight_layout()
-    plt.show()
+            ax.hist2d(filtered_dfs_test[feature], diff, bins=10,
+                      # norm='log',
+                      cmap='YlGn',
+                      vmax=400)
+            ax.set_ylabel('$y_{pred}$ - $\\tilde y$')
+            ax.set_xlabel(feature)
+        fig.suptitle(f"{args.target} = {tree.rearrange_equation_infix_notation()[1]}", fontsize=10,
+                     )
+        fig.tight_layout()
+        plt.show()
+    except Exception as e:
+        print(f'Error in drawing histogram {e}')
+        print(tree.rearrange_equation_prefix_notation(-1))
+        print(traceback.format_exc())
+        return {}
 
 
 def add_units(args, equation, filtered_dfs_train, proposed_equations, tree):
