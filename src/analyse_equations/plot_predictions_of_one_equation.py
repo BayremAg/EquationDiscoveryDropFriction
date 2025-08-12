69c4fc7a1bd2adfa0bc62df7066f662dc13688e7
jbrugger <wwjbrugger@gmail.com>
2025-08-12 11:44:56 +0200

    plot only first 20 datapoints from each dataset

--- src/analyse_equations/plot_predictions_of_one_equation.py
+++ src/analyse_equations/plot_predictions_of_one_equation.py
@@ -3,10 +3,13 @@ from pathlib import Path
 
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+from matplotlib.ticker import ScalarFormatter
 
 from definitions import ROOT_DIR
 from src.SyntaxTree.src.syntax_tree.config_syntax_tree import ConfigSyntaxTree
 from src.analyse_equations.config_analyse_equations import ConfigPlotBestEquation
+from src.analyse_equations.utils import get_first_key
 from src.equation_discovery.config_equations_for_each_dataset import ConfigEquationDiscovery
 from src.equation_discovery.evaluate_equation import map_equation_to_syntax_tree
 from src.equation_discovery.fit_constant import fit_constants
@@ -43,21 +46,27 @@ def run():
 
 
 def plot_prediction(args, filtered_dfs_test, filtered_dfs_train, tree):
+    args.plot_prediction_max_len_dataset = 20
+    filtered_dfs_test = get_short_and_sorted_df(args, filtered_dfs_test)
+    filtered_dfs_train= get_short_and_sorted_df(args, filtered_dfs_train)
+
     equation_infix = tree.rearrange_equation_infix_notation()[1]
     y_pred_train = tree.evaluate_subtree(-1, filtered_dfs_train)
     y_pred_test = tree.evaluate_subtree(-1, filtered_dfs_test)
     y_true_train = filtered_dfs_train['y'].to_numpy()
     y_true_test = filtered_dfs_test['y'].to_numpy()
-    fig, (ax1, ax2) = plt.subplots(figsize=(14, 10), nrows=2, sharex=True, sharey=True)
+    fig, (ax1, ax2) = plt.subplots(figsize=(10, 10), nrows=2, sharex=True, sharey=True)
     fig.suptitle(equation_infix)
+    ax1.ticklabel_format(axis= 'y', style='sci', scilimits=(0,0))
     ax1.set_title('Train Data')
     ax1.scatter(range(len(y_true_train)), y_true_train, label='true', s=1)
     ax1.scatter(range(len(y_pred_train)), y_pred_train, label='prediction', s=1)
     ax1.legend(loc='upper right')
     ax1.set_ylabel('Friction Force')
+
     for index in filtered_dfs_train.drop_duplicates(subset='Video ID').index:
         ax1.axvline(x=index, ymax=1, color='black', linewidth=1)
-        string = (f"{int(filtered_dfs_train.loc[index, 'Video ID'])} "
+        string = (#f"{int(filtered_dfs_train.loc[index, 'Video ID'])} "
                   f"{round(np.rad2deg(filtered_dfs_train.loc[index, 'tilt_angle']))}° "
                   f"{filtered_dfs_train.loc[index, 'excel_name']}")
         ax1.text(x=index,
@@ -73,7 +82,8 @@ def plot_prediction(args, filtered_dfs_test, filtered_dfs_train, tree):
     ax2.set_xlabel('Index in concatenated dataset')
     for index in filtered_dfs_test.drop_duplicates(subset='Video ID').index:
         ax2.axvline(x=index, ymax=1, color='black', linewidth=1)
-        string = (f"{int(filtered_dfs_test.loc[index, 'Video ID'])} "
+        string = (#f"{int(filtered_dfs_test.loc[index, 'Video ID'])} "
+                  f"{round(np.rad2deg(filtered_dfs_test.loc[index, 'tilt_angle']))}° "
                   f"{filtered_dfs_test.loc[index, 'excel_name']}")
         ax2.text(x=index,
                  y=0.00034,
@@ -86,6 +96,58 @@ def plot_prediction(args, filtered_dfs_test, filtered_dfs_train, tree):
     Path(save_path).parent.mkdir(exist_ok=True, parents=True)
     fig.savefig(save_path)
     plt.show()
+
+def sci_notation(x, pos):
+    return '{:.1e}'.format(x)
+
+def get_short_and_sorted_df(args, filtered_dfs_test):
+    short_data = {}
+    for index, row in filtered_dfs_test.iterrows():
+        if row['excel_name'] not in short_data:
+            short_data[row['excel_name']] = {}
+        if row['Video ID'] not in short_data[row['excel_name']]:
+            short_data[row['excel_name']][row['Video ID']] = {}
+        if (len(short_data[row['excel_name']][row['Video ID']]) <
+                args.plot_prediction_max_len_dataset):
+            short_data[row['excel_name']][row['Video ID']][index] = row
+    short_sorted_list = []
+    short_sorted_list_excel_name = []
+    current_excel_name = ''
+    tilt_angles = []
+    excel_name_list = list(short_data.keys())
+    excel_name_list.sort()
+    for excel_name in excel_name_list:
+        excel_dict = short_data[excel_name]
+        if current_excel_name != excel_name:
+            if len(tilt_angles)>0:
+                short_sorted_list = sort_videos_after_tilt_angle(
+                    short_sorted_list,
+                    short_sorted_list_excel_name,
+                    tilt_angles
+                )
+            current_excel_name = excel_name
+            tilt_angles = []
+            short_sorted_list_excel_name = []
+        for video_id, video_dict in excel_dict.items():
+            short_sorted_list_excel_name.append(pd.DataFrame(video_dict).transpose())
+            tilt_angles.append(video_dict[get_first_key(video_dict)]['tilt_angle'])
+    short_sorted_list = sort_videos_after_tilt_angle(
+        short_sorted_list,
+        short_sorted_list_excel_name,
+        tilt_angles
+    )
+
+    short_sorted_df = pd.concat(short_sorted_list)
+    short_sorted_df.reset_index(drop=True, inplace=True)
+    return short_sorted_df
+
+
+def sort_videos_after_tilt_angle(short_sorted_list, short_sorted_list_excel_name, tilt_angles):
+    sort_index = list(np.argsort(tilt_angles))
+    short_sorted_list_excel_name = (
+        [short_sorted_list_excel_name[index] for index in sort_index])
+    short_sorted_list += short_sorted_list_excel_name
+    return short_sorted_list
 
 
 if __name__ == '__main__':
