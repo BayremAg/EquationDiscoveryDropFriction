f60b3bc1f93aec3483959977acf5e5004e729151
jbrugger <wwjbrugger@gmail.com>
2026-01-16 12:54:40 +0100

    change ReMSe to ReRMSE

--- src/equation_discovery/rewards.py
+++ src/equation_discovery/rewards.py
@@ -1,14 +1,15 @@
 import numpy as np
 
 
-def ReMSe(y_pred, y_true):
+def ReRMSE(y_pred, y_true):
     # relative root-mean-square error as in [Brence, Todorovski, and Džeroski, “Probabilistic Grammars for Equation
-    # Discovery.”]
-    mean_y_true = np.mean(y_true)
-    mean_square_error = Mse(y_pred, y_true)
-    variance = np.mean(np.power(y_true - mean_y_true, 2))
-    remse = np.divide(np.sqrt(mean_square_error), variance + 10e-10)
-    return float(remse)
+    # Discovery.” page 5]
+    std = np.std(y_true)
+    mse = np.mean(np.power(y_pred - y_true, 2) )
+    rermse = np.sqrt(mse)/max(std,10e-18)
+
+    return float(rermse)
+
 
 
 def Mse(y_pred, y_true):
