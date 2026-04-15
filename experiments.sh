70ce4843a2011ec6da6020bf3aaedf41d5763d55
jbrugger <wwjbrugger@gmail.com>
2026-04-15 17:34:47 +0200

    clean up experiments.sh

--- experiments.sh
+++ experiments.sh
@@ -6,9 +6,10 @@ export PYTHONPATH=$PYTHONPATH:$(pwd)
 python src/equation_discovery/equations_for_each_dataset.py --number_of_runs 100 --num_mcts_sims  1000 --equation_discoverer MGMT
 
 # Run MGMT with a pretrained net
-/home/jbrugger/.virtualenvs/equation_discovery_drop_friction/bin/python3.10
-source ~/.virtualenvs/equation_discovery_drop_friction/bin/activate
- ~/.virtualenvs/equation_discovery_drop_friction/bin/python3.10 src/equation_discovery/equations_for_each_dataset.py  --number_of_runs 1 --num_mcts_sims  250_000 --num_rows_for_ed 500 --equation_discoverer MGMT  --constant_for_each_system True  --max_elements_in_list 500
+python src/equation_discovery/equations_for_each_dataset.py  --number_of_runs 1 --num_mcts_sims  250_000 --num_rows_for_ed 500 --equation_discoverer MGMT  --constant_for_each_system True  --max_elements_in_list 500
 
 # Run PySR
 python src/equation_discovery/equations_for_each_dataset.py --number_of_runs 10  --equation_discoverer PySR
+
+# Run analysis
+jupyter notebook src/analyse_equations/analyse_equations.ipynb
