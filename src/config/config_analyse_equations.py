c74a0844ec5a4de548337999f0ea793f3fc8de30
jbrugger <wwjbrugger@gmail.com>
2026-04-17 11:22:04 +0200

    rename : in time stings to - to be compatible with windows

--- src/config/config_analyse_equations.py
+++ src/config/config_analyse_equations.py
@@ -12,17 +12,17 @@ class ConfigPlotBestEquation:
         parser.add_argument('--paths_to_load_results', nargs='*',
                             help='List of strings',
                             default=[
-                                'results/Sajjad_Smoothed/02_Jun_2025_23:47:32_best_models_NGED.json',
-                                'results/Sajjad/04_Apr_2025_09:07:37_best_models_1c_50_interactions.json',
-                                 'results/Xiaomei/28_Mär_2025_11:41:26_best_models_1c_50_interactions.json',
-                                'results/Xiaomei/28_Mär_2025_12:07:31_best_models_1c_50_interactions.json',
-                                'results/Xiaomei/01_Apr_2025_17:12:42_best_models_1c_50_interactions.json',
-                                'results/Xiaomei/01_Apr_2025_18:10:55_best_models_1c_50_interactions.json',
-                                 'results/Xiaomei/01_Apr_2025_20:15:19_best_models_1c_50_interactions.json',
-                                'results/Xiaomei/01_Apr_2025_20:45:29_best_models_1c_50_interactions.json',
-                                'results/Xiaomei/02_Apr_2025_18:35:38_best_models_1c_50_interactions.json',
-                                'results/Xiaomei/09_Apr_2025_11:53:01_best_models_1c_50_interactions.json',
-                                'results/Aug_2025/12_Aug_2025_16:45:00_best_models_ID_error_per_dataset.json',
+                                'results/Sajjad_Smoothed/02_Jun_2025_23-47-32_best_models_NGED.json',
+                                'results/Sajjad/04_Apr_2025_09-07-37_best_models_1c_50_interactions.json',
+                                 'results/Xiaomei/28_Mär_2025_11-41-26_best_models_1c_50_interactions.json',
+                                'results/Xiaomei/28_Mär_2025_12-07-31_best_models_1c_50_interactions.json',
+                                'results/Xiaomei/01_Apr_2025_17-12-42_best_models_1c_50_interactions.json',
+                                'results/Xiaomei/01_Apr_2025_18-10-55_best_models_1c_50_interactions.json',
+                                 'results/Xiaomei/01_Apr_2025_20-15-19_best_models_1c_50_interactions.json',
+                                'results/Xiaomei/01_Apr_2025_20-45-29_best_models_1c_50_interactions.json',
+                                'results/Xiaomei/02_Apr_2025_18-35-38_best_models_1c_50_interactions.json',
+                                'results/Xiaomei/09_Apr_2025_11-53-01_best_models_1c_50_interactions.json',
+                                'results/Aug_2025/12_Aug_2025_16-45-00_best_models_ID_error_per_dataset.json',
                             ])
 
 
