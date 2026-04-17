6b507a30e2ee7212c8b5999a3281a0aa443660b6
jbrugger <wwjbrugger@gmail.com>
2026-04-17 11:39:20 +0200

    add log files in a windows compatible way

--- src/config/config_analyse_equations.py
+++ src/config/config_analyse_equations.py
@@ -14,8 +14,8 @@ class ConfigPlotBestEquation:
                             default=[
                                 'results/Sajjad_Smoothed/02_Jun_2025_23-47-32_best_models_NGED.json',
                                 'results/Sajjad/04_Apr_2025_09-07-37_best_models_1c_50_interactions.json',
-                                 'results/Xiaomei/28_Mär_2025_11-41-26_best_models_1c_50_interactions.json',
-                                'results/Xiaomei/28_Mär_2025_12-07-31_best_models_1c_50_interactions.json',
+                                 'results/Xiaomei/28_March_2025_11-41-26_best_models_1c_50_interactions.json',
+                                'results/Xiaomei/28_March_2025_12-07-31_best_models_1c_50_interactions.json',
                                 'results/Xiaomei/01_Apr_2025_17-12-42_best_models_1c_50_interactions.json',
                                 'results/Xiaomei/01_Apr_2025_18-10-55_best_models_1c_50_interactions.json',
                                  'results/Xiaomei/01_Apr_2025_20-15-19_best_models_1c_50_interactions.json',
